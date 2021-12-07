37067d65ca71ce31dcb69b229fbb8bbb9c0e99e6
x1n32 <x1n32@students.keele.ac.uk>
2021-12-07 10:16:45 +0000

    Test Working

---
+++ Calculator/src/main/java/github/x1n32/App.java
@@ -0,0 +1,13 @@
+package github.x1n32;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
