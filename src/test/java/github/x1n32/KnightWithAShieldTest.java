50780e04037e4acf062599d574f54e45d70102d5
x1n32 <x1n32@students.keele.ac.uk>
2022-03-31 13:05:21 +0000

    test msg

--- src/test/java/github/x1n32/KnightWithAShieldTest.java
+++ src/test/java/github/x1n32/KnightWithAShieldTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-//import org.junit.rules.ErrorCollector;
 
 public class KnightWithAShieldTest {
 
