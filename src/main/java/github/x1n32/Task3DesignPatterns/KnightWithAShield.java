152744bbbd4e5bb5819ea730b96ed71bb9ad59a7
x1n32 <77933595+x1n32@users.noreply.github.com>
2022-04-13 01:12:34 +0100

    Update KnightWithAShield.java

--- src/main/java/github/x1n32/Task3DesignPatterns/KnightWithAShield.java
+++ src/main/java/github/x1n32/Task3DesignPatterns/KnightWithAShield.java
@@ -2,12 +2,12 @@ package github.x1n32.Task3DesignPatterns;
 
 
 /** TASK:
- * Create a KnightWithAShield.java class, that extends knightClass and returns " + shield " when the job function is called.
+ * Create a KnightWithAShield.java class, that returns " + shield " when the job function is called.
  * Make KnightWithAShield.java class provide 10 extra defence.
  */
 
-/* DELETE THIS LINE AND THE LINE AT THE BOTTOM.
-public class KnightWithAShield extends __________  {
+
+public class KnightWithAShield extends KnightClass{
     
     public KnightWithAShield(Character knight) {   
         super(knight);  
@@ -19,11 +19,9 @@ public class KnightWithAShield extends __________  {
     private String decorateWithShield() {
         return " + ______ "; //return " + shield " here.
     }
+    
 
-    @Override
-    public int def() {
-        return null; //Increase defence by 10 here. Hint use 'super' to call on the inherited def value
-    }
+
 
 }
-*///DELETE THIS LINE 
+ 
