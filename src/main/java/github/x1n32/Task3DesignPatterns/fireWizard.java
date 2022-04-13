6ac699afdc5e3639ab746453390fb1f66e7447a7
x1n32 <77933595+x1n32@users.noreply.github.com>
2022-04-13 01:10:36 +0100

    Update fireWizard.java
    Changed to avoid errors when coding other tasks

--- src/main/java/github/x1n32/Task3DesignPatterns/fireWizard.java
+++ src/main/java/github/x1n32/Task3DesignPatterns/fireWizard.java
@@ -2,26 +2,15 @@ package github.x1n32.Task3DesignPatterns;
 
 //create a "fireWizard.java" that extends WizardDecorator. Make magicSpell() output "Casting: Fireball!"
 
-/* REMOVE THIS LINE AND THE ONE AT THE BOTTOM
-public class fireWizard _______ _______________ {
 
-    /**
-     * Takes character and a name variable
-     * @param wizard
-     * @param name
-     */
+public class fireWizard extends WizardDecorator {
+
+
     public fireWizard(Character wizard, String name) {
         super(wizard, name);
     }
 
-    /**
-     * Returns Casting: Fireball!
-     */
-    @Override
-    public String magicSpell(){ 
-        return null; //TO DO: make this output "Casting: Fireball". Hint: use super to call 'Casting: ' from the decorator class.
-    }
+    //TO DO: override magic spell to make it return "Casting: Fireball". Hint: use super to call 'Casting: ' from the decorator class.
     
 }
 
-*/ //REMOVE THIS LINE
